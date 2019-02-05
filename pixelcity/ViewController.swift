372a49700eef2ab85baa5cbf8385820ef3f6e34b
Tam Lam <imtamtlam@gmail.com>
2019-02-05 19:33:58 +0700

    Initial Commit

---
+++ pixelcity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pixelcity
+//
+//  Created by Graphene on 5/2/19.
+//  Copyright © 2019 tam-lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
