3c118782b0a537a1d2996d8b81c3e4d69f3543a6
Tam Lam <imtamtlam@gmail.com>
2019-02-06 21:45:28 +0700

    photo collection view for pullUpView

--- pixelcity/Controller/MapVC.swift
+++ pixelcity/Controller/MapVC.swift
@@ -16,7 +16,9 @@ class MapVC: UIViewController , UIGestureRecognizerDelegate{
     
     var spinner : UIActivityIndicatorView?
     var progressLbl: UILabel?
+    var collectionView : UICollectionView?
     
+    var flowLayout = UICollectionViewFlowLayout()
     var screenSize = UIScreen.main.bounds
     
     let authorizationStatus = CLLocationManager.authorizationStatus()
@@ -31,6 +33,13 @@ class MapVC: UIViewController , UIGestureRecognizerDelegate{
         configureLocationServices()
         locationManager.startUpdatingLocation()
 
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+        
+        pullUpView.addSubview(collectionView!)
     }
     func addDoubleTap(){
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(MapVC.dropPin))
@@ -66,12 +75,30 @@ class MapVC: UIViewController , UIGestureRecognizerDelegate{
     
     func addSpinner(){
         spinner = UIActivityIndicatorView()
-        var xPoint =  (screenSize.width / 2) - (spinner?.frame.width)!/2
+        let xPoint =  (screenSize.width / 2) - (spinner?.frame.width)!/2
         spinner?.center = CGPoint(x: xPoint, y: 150)
         spinner?.style = .whiteLarge
         spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         spinner?.startAnimating()
-        pullUpView.addSubview(spinner!)
+        collectionView!.addSubview(spinner!)
+    }
+    func removeSpinner(){
+        if spinner != nil{
+            spinner?.removeFromSuperview()
+        }
+    }
+    func addProgressLbl(){
+        progressLbl = UILabel()
+        progressLbl?.frame = CGRect(x: (screenSize.width / 2) - 120, y: 175, width: 240, height: 40)
+        progressLbl?.font = UIFont(name: "Avenir Next", size: 18)
+        progressLbl?.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        progressLbl?.textAlignment = .center
+        collectionView!.addSubview(progressLbl!)
+    }
+    func removeProgressLbl(){
+        if progressLbl != nil{
+            progressLbl?.removeFromSuperview()
+        }
     }
     @IBAction func centerMapwasPressed(_ sender: Any) {
         if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse{
@@ -97,10 +124,14 @@ extension MapVC : MKMapViewDelegate{
     }
     
     @objc func dropPin(sender: UITapGestureRecognizer){
+        removeSpinner()
+        removeProgressLbl()
         removePin()
         animateViewUp()
         addSwipe()
         addSpinner()
+        addProgressLbl()
+        progressLbl?.text = "12 PHOTOS LOADED"
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
@@ -135,3 +166,15 @@ extension MapVC : CLLocationManagerDelegate{
     }
 }
 
+extension MapVC : UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell:  PhotoCell  = ((collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell)!)
+        return cell
+    }
+    
+    
+}
---
+++ pixelcity/View/PhotoCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCell.swift
+//  pixelcity
+//
+//  Created by Graphene on 6/2/19.
+//  Copyright © 2019 tam-lam. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init coder error")
+    }
+}
